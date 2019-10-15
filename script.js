10323b196c3763e65ef277ea975f1d9e66a81179
bisesa1920daw2 <bisesa1920daw2@gmail.com>
2019-10-15 21:41:10 +0200

    Afegit dissenys CSS, sobretot per index.html

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ let canvas;
 let ctx;
 let mouse;
 
+let events = [];
+
 window.addEventListener('load', () => {
     // comprova si el navegador és compatible amb canvas
     try {
@@ -14,47 +16,87 @@ window.addEventListener('load', () => {
     canvas = document.getElementById('area-dibuix'); 
     ctx = canvas.getContext('2d');
 
+    // valors per defecte pel tipus de traç
+    ctx.strokeStyle = '#000';
+    ctx.lineWidth = 3;
+    ctx.lineJoin = 'round';
+    ctx.lineCap = 'round';  
+
     let painting = document.getElementById('paint');
     let paint_style = getComputedStyle(painting);
     canvas.width = parseInt(paint_style.getPropertyValue('width'));
     canvas.height = parseInt(paint_style.getPropertyValue('height'));
 
+    // variable que recull les coordinades del ratolí
     mouse = { x: 0, y: 0 };
+    
+    // assignació de event listeners per les eines
+    document.getElementById('btn-pincell').addEventListener('click', einaPincell);
+    document.getElementById('btn-cercle').addEventListener('click', einaCercle);
+    document.getElementById('btn-rectangle').addEventListener('click', einaRectangle);
+    document.getElementById('btn-color-pick').addEventListener('change', canviaColor); 
 
+    // simula un click al botó del pincell perquè és l'eina per defecte
+    document.getElementById('btn-pincell').click();
+}, true);
+
+// funcions
+
+let getCursorPosition = () => {
+    let rect = canvas.getBoundingClientRect();
+    mouse.x = event.clientX - rect.left;
+    mouse.y = event.clientY - rect.top;
+}; 
+
+let canviaColor = () => {
+    ctx.strokeStyle = document.getElementById('btn-color-pick').value;
+};
+
+let pintaPunt = () => {
+    ctx.lineTo(mouse.x, mouse.y);
+    ctx.stroke();
+};
+
+let einaPincell = () => {
     // event mousemove per pintar amb el pincell només mentre es mou el cursor
-    canvas.addEventListener('mousemove', (e) => getCursorPosition(), false);
+    events.push(canvas.addEventListener('mousemove', (e) => getCursorPosition(), false));
+    console.log(`events count: ${events.length}`);
 
-    // assignació de les propietats al pincell
-    ctx.lineWidth = 3;
-    ctx.lineJoin = 'round';
-    ctx.lineCap = 'round';
-    ctx.strokeStyle = '#000';
-
-    canvas.addEventListener('mousedown', (e) => {
+    events.push(canvas.addEventListener('mousedown', (e) => {
         ctx.beginPath();
         ctx.moveTo(mouse.x, mouse.y);
 
-        canvas.addEventListener('mousemove', onPaint, false);
-    }, false);
+        canvas.addEventListener('mousemove', pintaPunt, false);
+    }, false));
 
-    canvas.addEventListener('mouseup', () => {
-        canvas.removeEventListener('mousemove', onPaint, false);
-    }, false);
+    console.log(`events count: ${events.length}`);
 
-    let onPaint = () => {
-        ctx.lineTo(mouse.x, mouse.y);
-        ctx.stroke();
-    };
-
-    let getCursorPosition = () => {
-        let rect = canvas.getBoundingClientRect();
-        mouse.x = event.clientX - rect.left;
-        mouse.y = event.clientY - rect.top;
-    };  
-}, true);
-
-let einaPincell = () => {
-    
+    events.push(canvas.addEventListener('mouseup', () => {
+        canvas.removeEventListener('mousemove', pintaPunt, false);
+        console.log(`events count: ${events.length}`);
+    }, false));    
 };
 
-document.getElementById('btn-pincell').addEventListener('click', einaPincell);
+let einaCercle = () => {
+    if (events.length != 0) {
+        for (let e in events) {
+            canvas.removeEventListener('mouse')
+        }
+    }
+
+    canvas.addEventListener('mousedown', (e) => {
+        getCursorPosition();
+        ctx.beginPath();
+        ctx.arc(mouse.x, mouse.y, 50, 0, 2 * Math.PI);
+        ctx.stroke();
+    }, false);
+};
+
+let einaRectangle = () => {
+    canvas.addEventListener('mousedown', (e) => {
+        getCursorPosition();
+        ctx.beginPath();
+        ctx.rect(mouse.x, mouse.y, 150, 100);
+        ctx.stroke();
+    }, false);
+};

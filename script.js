0b7ef27c0f12ce85e86a33e90e71cfc15921aab0
bisesa1920daw2 <bisesa1920daw2@gmail.com>
2019-10-15 18:38:01 +0200

    Passada la funció inicialització a arrow

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-let inicialitzacio = () => {
+let canvas;
+let ctx;
+let mouse;
+
+window.addEventListener('load', () => {
     // comprova si el navegador és compatible amb canvas
     try {
         document.createElement('canvas').getContext('2d');
@@ -7,17 +11,15 @@ let inicialitzacio = () => {
     }
 
     // recull el canvas i el context del HTML
-    let canvas = document.getElementById('area-dibuix');
-    let ctx = canvas.getContext('2d');
+    canvas = document.getElementById('area-dibuix'); 
+    ctx = canvas.getContext('2d');
 
     let painting = document.getElementById('paint');
     let paint_style = getComputedStyle(painting);
     canvas.width = parseInt(paint_style.getPropertyValue('width'));
     canvas.height = parseInt(paint_style.getPropertyValue('height'));
 
-    let mouse = { x: 0, y: 0 };
-
-    console.log(`assignacio mouse: ${mouse.x} + ${mouse.y}`);
+    mouse = { x: 0, y: 0 };
 
     // event mousemove per pintar amb el pincell només mentre es mou el cursor
     canvas.addEventListener('mousemove', (e) => getCursorPosition(), false);
@@ -49,11 +51,10 @@ let inicialitzacio = () => {
         mouse.x = event.clientX - rect.left;
         mouse.y = event.clientY - rect.top;
     };  
-}
+}, true);
 
 let einaPincell = () => {
-
+    
 };
 
-window.addEventListener('load', inicialitzacio, true);
 document.getElementById('btn-pincell').addEventListener('click', einaPincell);
